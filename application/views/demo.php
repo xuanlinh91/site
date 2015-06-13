de6b6980867949041b7f9d3c6e0f3c24b3db75e0
xuanlinh91 <xuanlinh91@gmail.com>
2015-06-13 22:12:13 +0700

    update slider

--- application/views/demo.php
+++ application/views/demo.php
@@ -1,10 +1,41 @@
 <?php include('header.php');?>
-    <div class=".container-fluid well">
+    <div class="container-fluid well">
         <div class="container">
             <div class="row">
                 <div class="content">
                     <div class="col-lg-12 col-lg-offset-1">
                         <div class="row">
+                            <div class="box box-left col-lg-10">
+                                <?php
+//                                    include('slider/slider.php');
+                                ?>
+<!--                                --><?php //echo img(array('src'=>'/image/box-1.png', 'alt'=> 'alt information', 'width'=>'100%')); ?>
+                            </div>
+
+                            <div class="left-bar col-lg-7">
+                                <div class="row">
+                                    <div class="box box-left col-lg-7">
+                                        <?php echo img(array('src'=>'/image/box-1.png', 'alt'=> 'alt information', 'width'=>'100%')); ?>
+                                    </div>
+                                    <div class="box box-center col-lg-5">
+                                        <?php echo img(array('src'=>'/image/box-2.png', 'alt'=> 'alt information', 'width'=>'100%')); ?>
+                                    </div>
+                                    <div class="box box-left col-lg-12">
+                                        <?php echo img(array('src'=>'/image/box-3.png', 'alt'=> 'alt information', 'width'=>'100%')); ?>
+                                    </div>
+                                    <div class="box box-center col-lg-7">
+                                        abcd
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="box sidebar col-lg-3">
+                                <div class="sidebar-box #1">
+                                    <?php echo img(array('src'=>'/image/side-bar.jpg', 'alt'=> 'alt information', 'width'=>'100%')); ?>
+                                </div>
+                                <div class="sidebar-box #2">
+                                    <?php echo img(array('src'=>'/image/box-4.png', 'alt'=> 'alt information', 'width'=>'100%')); ?>
+                                </div>
+                            </div>
                             <div class="box box-left col-lg-10">
                                 <?php
 //                                    include('slider/slider.php');
--- application/views/header.php
+++ application/views/header.php
@@ -15,7 +15,6 @@
     <script src="/site/js/jquery-1.11.3.min.js"></script>
     <script src="/site/js/bootstrap.min.js"></script>
     <script src="/site/js/script.js"></script>
-    <script src="/site/js/docs.min.js"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="/site/js/ie10-viewport-bug-workaround.js"></script>
     <script>
@@ -121,125 +120,131 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">Project name</a>
+                <a class="navbar-brand" href="#">English Blog & Article</a>
             </div>
             <div class="collapse navbar-collapse">
                 <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#contact">Contact</a></li>
+                    <li class="active"><a href="#">Article</a></li>
+                    <li><a href="#about">Blog</a></li>
+                    <li><a href="#contact">Training</a></li>
                 </ul>
             </div><!--/.nav-collapse -->
         </div>
     </div>
+</div>
+<div class="container-fluid">
     <div class="container">
+        <div class="row">
+            <div class="slider col-lg-10 col-lg-offset-1">
+                <div id="slider1_container" style="display: none; position: relative; margin: 0 auto; width: 1140px; height: 442px; overflow: hidden;">
+
+                    <!-- Loading Screen -->
+                    <div u="loading" style="position: absolute; top: 0px; left: 0px;">
+                        <div style="filter: alpha(opacity=70); opacity:0.7; position: absolute; display: block;
+
+                background-color: #000; top: 0px; left: 0px;width: 100%; height:100%;">
+                        </div>
+                        <div style="position: absolute; display: block; background: url(/site/image/slider/loading.gif) no-repeat center center;
+
+                top: 0px; left: 0px;width: 100%;height:100%;">
+                        </div>
+                    </div>
+
+                    <!-- Slides Container -->
+                    <div u="slides" style="cursor: move; position: absolute; left: 0px; top: 0px; width: 1140px; height: 442px;
+            overflow: hidden;">
+                        <div>
+                            <img u="image" src2="/site/image/slider/home/01.jpg" />
+                        </div>
+                        <div>
+                            <img u="image" src2="/site/image/slider/home/02.jpg" />
+                        </div>
+                        <div>
+                            <img u="image" src2="/site/image/slider/home/03.jpg" />
+                        </div>
+                        <div>
+                            <img u="image" src2="/site/image/slider/home/04.jpg" />
+                        </div>
+                    </div>
+
+                    <!--#region Bullet Navigator Skin Begin -->
+                    <!-- Help: http://www.jssor.com/development/slider-with-bullet-navigator-jquery.html -->
+                    <style>
+                        /* jssor slider bullet navigator skin 05 css */
+                        /*
+                        .jssorb05 div           (normal)
+                        .jssorb05 div:hover     (normal mouseover)
+                        .jssorb05 .av           (active)
+                        .jssorb05 .av:hover     (active mouseover)
+                        .jssorb05 .dn           (mousedown)
+                        */
+                        .jssorb05 {
+                            position: absolute;
+                        }
+                        .jssorb05 div, .jssorb05 div:hover, .jssorb05 .av {
+                            position: absolute;
+                            /* size of bullet elment */
+                            width: 16px;
+                            height: 16px;
+                            background: url(/site/image/slider/b05.png) no-repeat;
+                            overflow: hidden;
+                            cursor: pointer;
+                        }
+                        .jssorb05 div { background-position: -7px -7px; }
+                        .jssorb05 div:hover, .jssorb05 .av:hover { background-position: -37px -7px; }
+                        .jssorb05 .av { background-position: -67px -7px; }
+                        .jssorb05 .dn, .jssorb05 .dn:hover { background-position: -97px -7px; }
+                    </style>
+                    <!-- bullet navigator container -->
+                    <div u="navigator" class="jssorb05" style="bottom: 16px; right: 6px;">
+                        <!-- bullet navigator item prototype -->
+                        <div u="prototype"></div>
+                    </div>
+                    <!--#endregion Bullet Navigator Skin End -->
+
+                    <!--#region Arrow Navigator Skin Begin -->
+                    <!-- Help: http://www.jssor.com/development/slider-with-arrow-navigator-jquery.html -->
+                    <style>
+                        /* jssor slider arrow navigator skin 11 css */
+                        /*
+                        .jssora11l                  (normal)
+                        .jssora11r                  (normal)
+                        .jssora11l:hover            (normal mouseover)
+                        .jssora11r:hover            (normal mouseover)
+                        .jssora11l.jssora11ldn      (mousedown)
+                        .jssora11r.jssora11rdn      (mousedown)
+                        */
+                        .jssora11l, .jssora11r {
+                            display: block;
+                            position: absolute;
+                            /* size of arrow element */
+                            width: 37px;
+                            height: 37px;
+                            cursor: pointer;
+                            background: url(/site/image/slider/a11.png) no-repeat;
+                            overflow: hidden;
+                        }
+                        .jssora11l { background-position: -11px -41px; }
+                        .jssora11r { background-position: -71px -41px; }
+                        .jssora11l:hover { background-position: -131px -41px; }
+                        .jssora11r:hover { background-position: -191px -41px; }
+                        .jssora11l.jssora11ldn { background-position: -251px -41px; }
+                        .jssora11r.jssora11rdn { background-position: -311px -41px; }
+                    </style>
+                    <!-- Arrow Left -->
+            <span u="arrowleft" class="jssora11l" style="top: 123px; left: 8px;">
+            </span>
+                    <!-- Arrow Right -->
+            <span u="arrowright" class="jssora11r" style="top: 123px; right: 8px;">
+            </span>
+                    <!--#endregion Arrow Navigator Skin End -->
+                    <a style="display: none" href="http://www.jssor.com">Bootstrap Slider</a>
+                </div>
+            </div>
+        </div>
         <!-- Jssor Slider Begin -->
         <!-- To move inline styles to css file/block, please specify a class name for each element. -->
         <!-- ================================================== -->
-        <div id="slider1_container" style="display: none; position: relative; margin: 0 auto; width: 1140px; height: 442px; overflow: hidden;">
-
-            <!-- Loading Screen -->
-            <div u="loading" style="position: absolute; top: 0px; left: 0px;">
-                <div style="filter: alpha(opacity=70); opacity:0.7; position: absolute; display: block;
-
-                background-color: #000; top: 0px; left: 0px;width: 100%; height:100%;">
-                </div>
-                <div style="position: absolute; display: block; background: url(/image/slider/loading.gif) no-repeat center center;
-
-                top: 0px; left: 0px;width: 100%;height:100%;">
-                </div>
-            </div>
-
-            <!-- Slides Container -->
-            <div u="slides" style="cursor: move; position: absolute; left: 0px; top: 0px; width: 1140px; height: 442px;
-            overflow: hidden;">
-                <div>
-                    <img u="image" src2="/site/image/slider/home/01.jpg" />
-                </div>
-                <div>
-                    <img u="image" src2="/site/image/slider/home/02.jpg" />
-                </div>
-                <div>
-                    <img u="image" src2="/site/image/slider/home/03.jpg" />
-                </div>
-                <div>
-                    <img u="image" src2="/site/image/slider/home/04.jpg" />
-                </div>
-            </div>
-
-            <!--#region Bullet Navigator Skin Begin -->
-            <!-- Help: http://www.jssor.com/development/slider-with-bullet-navigator-jquery.html -->
-            <style>
-                /* jssor slider bullet navigator skin 05 css */
-                /*
-                .jssorb05 div           (normal)
-                .jssorb05 div:hover     (normal mouseover)
-                .jssorb05 .av           (active)
-                .jssorb05 .av:hover     (active mouseover)
-                .jssorb05 .dn           (mousedown)
-                */
-                .jssorb05 {
-                    position: absolute;
-                }
-                .jssorb05 div, .jssorb05 div:hover, .jssorb05 .av {
-                    position: absolute;
-                    /* size of bullet elment */
-                    width: 16px;
-                    height: 16px;
-                    background: url(/site/image/slider/b05.png) no-repeat;
-                    overflow: hidden;
-                    cursor: pointer;
-                }
-                .jssorb05 div { background-position: -7px -7px; }
-                .jssorb05 div:hover, .jssorb05 .av:hover { background-position: -37px -7px; }
-                .jssorb05 .av { background-position: -67px -7px; }
-                .jssorb05 .dn, .jssorb05 .dn:hover { background-position: -97px -7px; }
-            </style>
-            <!-- bullet navigator container -->
-            <div u="navigator" class="jssorb05" style="bottom: 16px; right: 6px;">
-                <!-- bullet navigator item prototype -->
-                <div u="prototype"></div>
-            </div>
-            <!--#endregion Bullet Navigator Skin End -->
-
-            <!--#region Arrow Navigator Skin Begin -->
-            <!-- Help: http://www.jssor.com/development/slider-with-arrow-navigator-jquery.html -->
-            <style>
-                /* jssor slider arrow navigator skin 11 css */
-                /*
-                .jssora11l                  (normal)
-                .jssora11r                  (normal)
-                .jssora11l:hover            (normal mouseover)
-                .jssora11r:hover            (normal mouseover)
-                .jssora11l.jssora11ldn      (mousedown)
-                .jssora11r.jssora11rdn      (mousedown)
-                */
-                .jssora11l, .jssora11r {
-                    display: block;
-                    position: absolute;
-                    /* size of arrow element */
-                    width: 37px;
-                    height: 37px;
-                    cursor: pointer;
-                    background: url(/site/image/slider/a11.png) no-repeat;
-                    overflow: hidden;
-                }
-                .jssora11l { background-position: -11px -41px; }
-                .jssora11r { background-position: -71px -41px; }
-                .jssora11l:hover { background-position: -131px -41px; }
-                .jssora11r:hover { background-position: -191px -41px; }
-                .jssora11l.jssora11ldn { background-position: -251px -41px; }
-                .jssora11r.jssora11rdn { background-position: -311px -41px; }
-            </style>
-            <!-- Arrow Left -->
-            <span u="arrowleft" class="jssora11l" style="top: 123px; left: 8px;">
-            </span>
-            <!-- Arrow Right -->
-            <span u="arrowright" class="jssora11r" style="top: 123px; right: 8px;">
-            </span>
-            <!--#endregion Arrow Navigator Skin End -->
-            <a style="display: none" href="http://www.jssor.com">Bootstrap Slider</a>
-        </div>
         <!-- Jssor Slider End -->
     </div>
 
